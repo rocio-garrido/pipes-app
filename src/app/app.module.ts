a8e16e9892d6d5814d3d0e3370b556ac5d4b42b7
Rocio Garrido <rocio.garrido@wefox.com>
2023-01-04 09:56:15 +0100

    PrimeNg module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
 
 @NgModule({
   declarations: [
@@ -10,8 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    CardModule
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
