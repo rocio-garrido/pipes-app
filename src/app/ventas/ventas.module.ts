4d8c92a9f39f873ff558f74e71c2c972aff9ef68
Rocio Garrido <rocio.garrido@wefox.com>
2023-03-01 09:57:23 +0100

    Rutas de nuestra aplicación

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BasicosComponent } from './pages/basicos/basicos.component';
+import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
+import { NumerosComponent } from './pages/numeros/numeros.component';
+import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NumerosComponent,
+    NoComunesComponent,
+    BasicosComponent,
+    OrdenarComponent
+  ],
+  exports: [
+    NumerosComponent,
+    NoComunesComponent,
+    BasicosComponent,
+    OrdenarComponent
+  ],
   imports: [
     CommonModule
   ]
