1c4a3dcf744f37543f1dcae8bd187bd0c4cf815b
Rocio Garrido <rocio.garrido@wefox.com>
2023-03-08 09:04:33 +0100

    UpperCase, LowerCase y TitleCase Pipes

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
 })
 export class BasicosComponent {
 
+  nombreLower: string = 'fernando';
+  nombreUpper: string = 'FERNANDO';
+  nombreCompleto: string = 'fErNando herRera';
 }
